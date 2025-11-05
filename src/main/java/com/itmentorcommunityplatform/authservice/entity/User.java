7ce8d39a411832491d45fe009e0aa520cd1b6f93
Egor250625 <ivanovegor700@gmail.com>
2025-11-05 12:38:47 +0300

    fix: add @Table annotation

--- src/main/java/com/itmentorcommunityplatform/authservice/entity/User.java
+++ src/main/java/com/itmentorcommunityplatform/authservice/entity/User.java
@@ -2,8 +2,10 @@ package com.itmentorcommunityplatform.authservice.entity;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
+@Table(name = "users")
 public class User {
     @Id
     private Integer id;

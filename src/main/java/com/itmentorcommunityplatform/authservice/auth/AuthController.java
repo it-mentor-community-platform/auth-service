3879b449ca5052ad811eac365b8e996295716036
Egor250625 <ivanovegor700@gmail.com>
2025-11-05 18:00:22 +0300

    fix: edit AuthController
    now it  is gives the answer through headers

--- src/main/java/com/itmentorcommunityplatform/authservice/auth/AuthController.java
+++ src/main/java/com/itmentorcommunityplatform/authservice/auth/AuthController.java
@@ -1,6 +1,7 @@
 package com.itmentorcommunityplatform.authservice.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,8 +16,10 @@ public class AuthController {
     private final TelegramAuthService telegramAuthService;
 
     @PostMapping("/by-telegram")
-    public ResponseEntity<AuthResponse> authenticate(@RequestBody String initData) {
+    public ResponseEntity<Void> authenticate(@RequestBody String initData) {
         AuthResponse response = telegramAuthService.authenticateByTelegram(initData);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.AUTHORIZATION,"Bearer " + response.accessToken())
+                .build();
     }
 }

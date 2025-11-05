f6864060f99151cb9be33e96fc7f4e9dd3dac1a9
Egor250625 <ivanovegor700@gmail.com>
2025-11-05 11:44:53 +0300

    fix: edit UserRepository extends CrudRepository

--- src/main/java/com/itmentorcommunityplatform/authservice/auth/UserRepository.java
+++ src/main/java/com/itmentorcommunityplatform/authservice/auth/UserRepository.java
@@ -1,37 +1,12 @@
 package com.itmentorcommunityplatform.authservice.auth;
 
 import com.itmentorcommunityplatform.authservice.entity.User;
-import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-@RequiredArgsConstructor
-public class UserRepository {
+public interface  UserRepository extends CrudRepository<User,Long> {
 
-    private final JdbcTemplate jdbcTemplate;
+    User findByTelegramUserId(Long telegramUserId);
 
-    public User findByTelegramUserId(Long telegramUserId) {
-
-        String sql = "SELECT id, telegram_user_id FROM users WHERE telegram_user_id = ?";
-
-        List<User> users = jdbcTemplate.query(sql, (rs, rowNum) -> {
-            User user = new User();
-            user.setId(rs.getInt("id"));
-            user.setTelegramUserId(rs.getLong("telegram_user_id"));
-            return user;
-        }, telegramUserId);
-
-        return users.isEmpty() ? null : users.get(0);
-    }
-
-
-    public User save(User user) {
-        String sql = "INSERT INTO users (telegram_user_id) VALUES (?) RETURNING id";
-        Integer id = jdbcTemplate.queryForObject(sql, Integer.class, user.getTelegramUserId());
-        user.setId(id);
-        return user;
-    }
 }

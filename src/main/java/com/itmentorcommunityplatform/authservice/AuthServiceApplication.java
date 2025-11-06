12ef9bef1ab23fb27166549583249b0b07b2dbb0
Egor250625 <ivanovegor700@gmail.com>
2025-11-06 17:46:47 +0300

    feat: add GlobalExceptionHandler and Dto

--- src/main/java/com/itmentorcommunityplatform/authservice/AuthServiceApplication.java
+++ src/main/java/com/itmentorcommunityplatform/authservice/AuthServiceApplication.java
@@ -3,13 +3,9 @@ package com.itmentorcommunityplatform.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-
 @SpringBootApplication
 public class AuthServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
     }
-
 }

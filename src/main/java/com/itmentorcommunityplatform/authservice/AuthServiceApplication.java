f0f90adc7a3bd9e89efd965d0460c913b0dba0a0
Egor250625 <ivanovegor700@gmail.com>
2025-11-09 12:03:29 +0300

    fix: test: remove unused http test

--- src/main/java/com/itmentorcommunityplatform/authservice/AuthServiceApplication.java
+++ src/main/java/com/itmentorcommunityplatform/authservice/AuthServiceApplication.java
@@ -3,6 +3,8 @@ package com.itmentorcommunityplatform.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.stream.DoubleStream;
+
 @SpringBootApplication
 public class AuthServiceApplication {
     public static void main(String[] args) {

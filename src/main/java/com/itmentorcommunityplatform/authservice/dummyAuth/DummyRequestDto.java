1a6a2e7ff8cc56fd49f09c24d0bcd220e702e65d
Egor250625 <ivanovegor700@gmail.com>
2025-11-16 14:16:34 +0300

    feat: add Schema header to dto classes

--- src/main/java/com/itmentorcommunityplatform/authservice/dummyAuth/DummyRequestDto.java
+++ src/main/java/com/itmentorcommunityplatform/authservice/dummyAuth/DummyRequestDto.java
@@ -1,12 +1,14 @@
 package com.itmentorcommunityplatform.authservice.dummyAuth;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
+@Schema(description = "Request DTO for dummy authentication")
 public record DummyRequestDto(
         @NotNull
         @JsonProperty("user_id")
--- src/main/java/com/itmentorcommunityplatform/authservice/internalUser/UserUpsertRequestDto.java
+++ src/main/java/com/itmentorcommunityplatform/authservice/internalUser/UserUpsertRequestDto.java
@@ -1,11 +1,14 @@
 package com.itmentorcommunityplatform.authservice.internalUser;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
+
+@Schema(description = "Request DTO for importing internal user")
 public record UserUpsertRequestDto(
         @NotNull
         @JsonProperty("telegram_user_id")
